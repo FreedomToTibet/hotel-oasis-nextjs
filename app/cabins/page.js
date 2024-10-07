f9481a8aec942b86ba3aed4f3f404da2313c5cab
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-10-07 02:17:14 -0700

    data and css are added

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,18 +1,14 @@
-import Link from 'next/link';
+export const metadata = {
+	title: "Cabins",
+};
 
 export default async function Page () {
-	const response = await fetch('https://jsonplaceholder.typicode.com/users');
-	const data = await response.json();
-
+	
 	return (
 		<div>
 			<h1>Cabins</h1>
 			<p>Find your perfect cabin</p>
-			<ul>
-				{data.map(user => (
-					<li key={user.id}>{user.name}</li>
-				))}
-			</ul>
+			
 		</div>
 	);
 };
