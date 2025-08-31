265b60c112c984bc858ef846512f390be9a7aff3
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-08-31 03:28:35 -0700

    Add user avatar and login flow to reservation
    Introduces a LoginMessage component and updates Reservation and Navigation to display user avatars when logged in. ReservationForm now shows user info and avatar, and next.config.mjs allows images from Googleusercontent for profile pictures. Unauthenticated users are prompted to log in before reserving.

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,28 +1,49 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import { auth } from '@/app/_lib/auth';
 
-export default function Navigation() {
+export default async function Navigation() {
+	const session = await auth();
+
 	return (
-    <nav className="z-10 text-xl">
-      <ul className="flex gap-16 items-center">
-        <li>
-          <Link href="/cabins" className="hover:text-accent-400 transition-colors">
-            Cabins
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" className="hover:text-accent-400 transition-colors">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+		<nav className="z-10 text-xl">
+			<ul className="flex gap-16 items-center">
+				<li>
+					<Link href="/cabins" className="hover:text-accent-400 transition-colors">
+						Cabins
+					</Link>
+				</li>
+				<li>
+					<Link href="/about" className="hover:text-accent-400 transition-colors">
+						About
+					</Link>
+				</li>
+				<li>
+					{session?.user?.image ? (
+						<Link
+							href="/account"
+							className="hover:text-accent-400 transition-colors flex items-center gap-2"
+						>
+							<Image
+								src={session.user.image}
+								alt="User Avatar"
+								width={32}
+								height={32}
+								referrerPolicy='no-referrer'
+								className="rounded-full"
+							/>
+							Guest area
+						</Link>
+					) : (
+						<Link
+							href="/account"
+							className="hover:text-accent-400 transition-colors"
+						>
+							Guest area
+						</Link>
+					)}
+				</li>
+			</ul>
+		</nav>
+	);
 }
