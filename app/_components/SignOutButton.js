bd21d3d5b1a4f2d84e62c0aaab263f4b45376638
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-09-01 01:42:07 -0700

    Implement custom login page and update auth flow
    Added a dedicated login page and updated authentication logic to use custom sign-in and sign-out actions. The account page now greets users by their first name, and authentication callbacks and page redirects have been configured for improved user experience.

--- app/_components/SignOutButton.js
+++ app/_components/SignOutButton.js
@@ -1,11 +1,14 @@
 import { ArrowLeftStartOnRectangleIcon } from '@heroicons/react/24/outline';
+import { signOutAction } from "@/app/_lib/actions";
 
 function SignOutButton() {
   return (
-    <button className='py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 w-full'>
+		<form action={signOutAction} method="POST">
+			<button className='py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 w-full'>
       <ArrowLeftStartOnRectangleIcon className='h-5 w-5 text-primary-600' />
       <span>Sign out </span>
     </button>
+		</form>
   );
 }
 
--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,16 @@
+import { auth } from "@/app/_lib/auth";
+
 export const metadata = {
 	title: "Guest area",
 	description: "Welcome to our luxury cabins!",
 };
 
-export default function Page () {
+export default async function Page () {
+	const session = await auth();
+	const firstName = session?.user?.name?.split(" ").at(0);
 	return (
 		<h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome, dear guest!
+        Welcome, dear guest {firstName}!
     </h2>
 	);
 };
